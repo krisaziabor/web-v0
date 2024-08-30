74cf7dfe69e1ff148dc3a0bf6c52f8f1a2fff0bc
kris aziabor <krisaaziabor@gmail.com>
2024-08-30 18:38:54 -0400

    changed metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Kristopher Aziabor",
+  title: "Kris Aziabor Dot Com Coming Soon",
   description: "Coming Soon",
   
 };
